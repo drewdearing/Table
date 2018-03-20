f77ea14f8df312faf79c706172ff9c895e8d96db
Emily Kwong <emilyk1217@gmail.com>
2018-03-20 11:55:24 -0500

    log in

--- app/src/main/java/com/example/emily/table/Login.java
+++ app/src/main/java/com/example/emily/table/Login.java
@@ -3,6 +3,7 @@ package com.example.emily.table;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
@@ -12,6 +13,7 @@ import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 import java.util.Arrays;
 
@@ -19,6 +21,8 @@ import java.util.Arrays;
 public class Login extends AppCompatActivity {
 
     protected CallbackManager callbackManager;
+    protected LoginButton loginButton;
+    private static final String EMAIL = "email";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,29 +32,61 @@ public class Login extends AppCompatActivity {
         AppEventsLogger.activateApp(this);
 
         callbackManager = CallbackManager.Factory.create();
-        boolean loggedIn = AccessToken.getCurrentAccessToken() == null;
+
+        boolean loggedIn = AccessToken.getCurrentAccessToken() != null;
         if (loggedIn) {
             LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
+            Intent goHome = new Intent(getApplicationContext(), Home.class);
+            startActivity(goHome);
         }
-        LoginManager.getInstance().registerCallback(callbackManager,
-                new FacebookCallback<LoginResult>() {
-                    @Override
-                    public void onSuccess(LoginResult loginResult) {
-                        // App code
-                        Intent goHome = new Intent(getApplicationContext(), Home.class);
-                        startActivity(goHome);
-                    }
+//        LoginManager.getInstance().registerCallback(callbackManager,
+//                new FacebookCallback<LoginResult>() {
+//                    @Override
+//                    public void onSuccess(LoginResult loginResult) {
+//                        // App code
+//                        Intent goHome = new Intent(getApplicationContext(), Home.class);
+//                        startActivity(goHome);
+//                    }
+//
+//                    @Override
+//                    public void onCancel() {
+//                        // App code
+//                    }
+//
+//                    @Override
+//                    public void onError(FacebookException exception) {
+//                        // App code
+//                    }
+//                });
 
-                    @Override
-                    public void onCancel() {
-                        // App code
-                    }
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList(EMAIL));
 
-                    @Override
-                    public void onError(FacebookException exception) {
-                        // App code
-                    }
-                });
+
+
+        // If you are using in a fragment, call loginButton.setFragment(this);
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+//                Intent goHome = new Intent(getApplicationContext(), Home.class);
+//                startActivity(goHome);
+                TextView hello = findViewById(R.id.textView);
+                hello.setText(EMAIL);
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
 
     }
 
